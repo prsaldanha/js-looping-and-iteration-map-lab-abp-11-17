8904f439e83b1f3978f31e61983b91e1642bcc3d
prem_saldanha@yahoo.ca <prem_saldanha@yahoo.ca>
2017-11-19 20:42:33 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file.
+function lowerCaseDrivers(collection)
+{
+  var lowers = collection.map(function(x) { return x.toLowerCase(); });
+  return lowers;
+}
+
+function nameToAttributes(collection)
+{
+  const newCollection = collection.map(
+      function (item)
+      {
+        var newItem = item.split(" ");
+        return Object.assign({}, { firstName: newItem[0], lastName: newItem[1] });
+      }
+    );
+
+  return newCollection;
+}
+
+function attributesToPhrase(collection)
+{
+  const newCollection = collection.map(
+      function (item)
+      {
+        return `${item.name} is from ${item.hometown}`;
+      }
+    );
+
+  return newCollection;
+}
